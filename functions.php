e05eb918f2921e3180a69829753b00eb8a8d15b6
Derek Chan <dchan3@hawaii.edu>
2019-05-19 06:49:56 +0000

    Fix functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
+<?php
+
 function init_scripts() {
   wp_enqueue_script('custom-react-theme-script', get_template_directory_uri() . '/dist/app.js' , array(), '1.0', true);
 }
 
-add_action('wp_enqueue_scripts, 'init_scripts');
+add_action('wp_enqueue_scripts', 'init_scripts');
+?>
