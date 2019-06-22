21e6dd3d57c189d0bd8b2957a206c6e8c773f696
dchan3 <dchan3@hawaii.edu>
2019-06-21 15:58:19 -1000

    Podcast PHP

--- functions.php
+++ functions.php
@@ -198,9 +198,15 @@ add_action('rest_api_init', function() {
       }
      ));
 
-    register_rest_field('podcast', 'playlist_id', array(
+    register_rest_field('podcast', 'podcast_id', array(
       'get_callback' => function($obj) {
-            return get_post_meta($obj['playlist_id']);
+            return get_post_meta($obj['id'], 'podcast_id');
+      }
+     ));
+
+    register_rest_field('podcast', 'podcast_type', array(
+      'get_callback' => function($obj) {
+            return get_post_meta($obj['id'], 'podcast_type');
       }
      ));
 });
