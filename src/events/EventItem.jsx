baf814523c7840bdf684fea8fd5b79e78f4ed381
josephpalma <joepalma08@hotmail.com>
2019-07-17 18:12:12 -1000

    parse url

--- src/events/EventItem.jsx
+++ src/events/EventItem.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
+
+function parseUrl(description) {
+  let reg = '/(https?:\/\/)?[a-z0-9.]+(\/[^\s\t]*)?/';
+  let url = description.match(reg);
+  if (url) {
+    return url[0];
+  }
+  return undefined;
+}
+
+
 function EventItem({ item: event }) {
   return <div className='news-list__post-parent'>
     <h3 className="home__section">{event.summary} | {event.start.dateTime} - {event.end.dateTime}</h3>
-    <a href={event.description} className='home__more'>
+    <a href={parseUrl(event.description) || '#'} className='home__more'>
       {event.summary}{'  '}
     </a>
     <div className='event_title'>
-      {event.location} |
+      {event.location} | {url}
     </div>
   </div>;
 }
