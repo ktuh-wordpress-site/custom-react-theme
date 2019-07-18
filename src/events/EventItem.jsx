d729040a0ddc2dbee0e45e090d236fe82bbddcb6
dchan3 <dchan3@hawaii.edu>
2019-07-18 08:22:31 -1000

    Events item link fix

--- src/events/EventItem.jsx
+++ src/events/EventItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { object } from 'prop-types';
 
-
 function parseUrl(description) {
-  let reg = '/(https?:\/\/)?[a-z0-9.]+(\/[^\s\t]*)?/';
-  let url = description.match(reg);
-  if (url) {
-    return url[0];
+  if (description) {
+    let reg = /(https?:\/\/)?[a-z0-9.]+(\/[^\s\t]*)?/;
+    let url = description.match(reg);
+    if (url) {
+      return url[0];
+    }
   }
   return undefined;
 }
@@ -15,11 +16,11 @@ function parseUrl(description) {
 function EventItem({ item: event }) {
   return <div className='news-list__post-parent'>
     <h3 className="home__section">{event.summary} | {event.start.dateTime} - {event.end.dateTime}</h3>
-    <a href={parseUrl(event.description) || '#'} className='home__more'>
+    <a href={event.description && parseUrl(event.description) || '#'} className='home__more'>
       {event.summary}{'  '}
     </a>
     <div className='event_title'>
-      {event.location} | {url}
+      {event.location} | {parseUrl(event.description)}
     </div>
   </div>;
 }
