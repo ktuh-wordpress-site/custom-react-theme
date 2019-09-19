12a2e9cbde1ae85db4059da8630860f61b4eb9aa
josephpalma <joepalma08@hotmail.com>
2019-09-18 16:27:00 -1000

    Staff page updates - doesnt display posts correctly

--- src/staffmembers/StaffMemberItem.jsx
+++ src/staffmembers/StaffMemberItem.jsx
@@ -5,7 +5,7 @@ import { getFullUrl, getFeaturedImg } from '../utils/url_utils';
 
 function NewsItem({
   item: {
-    _embedded, slug, membername, memberphoto, memberbio, memberrole
+    _embedded, slug, member_name, member_photo, member_bio, member_role
   }
 }) {
   let featuredImage = getFeaturedImg(_embedded),
@@ -14,14 +14,14 @@ function NewsItem({
   return <div className='news-list__post'><div className='news-list__post-image'>
     <SamePageAnchor className="news-list__photo-link"
       href={postUrl}>
-      <img className='news-list__photo' src={memberphoto} />
+      <img className='news-list__photo' src={member_photo} />
       </SamePageAnchor>
     </div>
     <div className='news-list__info'>
-      <SamePageAnchor className='news-list__title' href={postUrl}><h3>{membername}</h3>
+      <SamePageAnchor className='news-list__title' href={postUrl}><h3>{member_name}</h3>
       </SamePageAnchor>
-      <SamePageAnchor className='news-list__excerpt' href={postUrl}><h4>{memberrole}</h4></SamePageAnchor>
-      <SamePageAnchor className='news-list__excerpt' href={postUrl}><h5>{memberbio}</h5></SamePageAnchor>
+      <SamePageAnchor className='news-list__excerpt' href={postUrl}><h4>{member_role}</h4></SamePageAnchor>
+      <SamePageAnchor className='news-list__excerpt' href={postUrl}><h5>{member_bio}</h5></SamePageAnchor>
     </div>
   </div>;
 }
