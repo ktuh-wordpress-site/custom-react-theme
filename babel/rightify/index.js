4f4f023c2941b910629ad0dba683708242dbf52f
Derek Chan <dchan3@hawaii.edu>
2019-10-04 13:45:02 -1000

    Even more optos

--- babel/rightify/index.js
+++ babel/rightify/index.js
@@ -7,15 +7,20 @@ module.exports = function () {
         path.node.argument = t.stringLiteral('');
       },
       CallExpression(path) {
-        if (path.node.callee && path.node.callee.object
-            && path.node.callee.object.name === 'console'
-            && path.node.callee.property
+        if (path.node.callee && path.node.callee.object && path.node.callee.property) {
+          if (path.node.callee.object.name === 'console'
             && path.node.callee.property.name === 'error') {
-          path.node.arguments = [t.stringLiteral('')];
+            path.node.arguments = [t.stringLiteral('')];
+          }
+          else if (path.node.callee.object.type === 'ThisExpression'
+            && path.node.callee.property.name === '__error') {
+            path.node.arguments = [t.stringLiteral('')];
+          }
         }
       },
       NewExpression(path) {
-        if (path.node.callee && path.node.callee.name && path.node.callee.name === 'Error') {
+        if (path.node.callee && path.node.callee.name &&
+          path.node.callee.name.endsWith('Error')) {
           path.node.arguments = [t.stringLiteral('')];
         }
       }
