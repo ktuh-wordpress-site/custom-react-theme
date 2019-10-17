5866fc0c69f24929285149951a4c9fbf330c057e
Derek Chan <dchan3@hawaii.edu>
2019-10-17 11:55:31 -1000

    Delete CSV package whose parse function we only used

--- src/charts/ChartTable.jsx
+++ src/charts/ChartTable.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { default as parse } from 'csv-parse/lib/es5/sync';
+
+function parse(data) {
+  let retval = [], lines = data.split('\n');
+  for (let l = 0; l < lines.length; l++) {
+    if (lines[l].length) retval.push(lines[l].split(/(?<!\\),/));
+  }
+  return retval;
+}
 
 export default function ChartTable({ data }) {
-  let tableVals = parse(data, { bom: true, columns: true });
+  let tableVals = parse(data);
 
-  let header = Object.keys(tableVals[0]);
+  let [header, ...body] = tableVals;
 
   return <table>
     <thead>
       <tr>{header.map(col => <td>{col}</td>)}</tr>
     </thead>
     <tbody>
-      {tableVals.map(row => <tr>{Object.keys(row).map(col => <td>{row[col]}</td>)}</tr>)}
+      {body.map(row => <tr>{row.map(col => <td>{col}</td>)}</tr>)}
     </tbody>
   </table>;
 }
