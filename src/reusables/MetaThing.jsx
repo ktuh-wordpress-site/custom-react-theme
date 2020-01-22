ea6aff9874c062db058fe79a3e0592120416aa03
Derek Chan <dchan3@hawaii.edu>
2020-01-22 13:33:38 -1000

    Show null check, meta tags, and router issues

--- src/reusables/MetaThing.jsx
+++ src/reusables/MetaThing.jsx
@@ -3,38 +3,38 @@ import { getImgAsset } from '../utils/url_utils';
 
 let array = [
   {
-    selector: 'meta[property="description"]',
-    append: content => `<meta property="description" content=${content}>`,
+    selector: 'meta[name="description"]',
+    append: content => `<meta name="description" content="${content}">`,
     isFor: 'description'
   },
   {
     selector: 'meta[property="og:title"]',
-    append: content => `<meta property="og:title" content=${content}>`,
+    append: content => `<meta property="og:title" content="${content}">`,
     isFor: 'title'
   },
   {
     selector: 'meta[property="og:image"]',
-    append: content => `<meta property="og:image" content=${content}>`,
+    append: content => `<meta property="og:image" content="${content}">`,
     isFor: 'image'
   },
   {
     selector: 'meta[property="og:description"]',
-    append: content => `<meta property="og:description" content=${content}>`,
+    append: content => `<meta property="og:description" content="${content}">`,
     isFor: 'description'
   },
   {
     selector: 'meta[name="twitter:title"]',
-    append: content => `<meta name="twitter:title" content=${content}>`,
+    append: content => `<meta name="twitter:title" content="${content}">`,
     isFor: 'title'
   },
   {
     selector: 'meta[name="twitter:image"]',
-    append: content => `<meta name="twitter:image" content=${content}>`,
+    append: content => `<meta name="twitter:image" content="${content}">`,
     isFor: 'image'
   },
   {
     selector: 'meta[name="twitter:description"]',
-    append: content => `<meta name="twitter:description" content=${content}>`,
+    append: content => `<meta name="twitter:description" content="${content}">`,
     isFor: 'description'
   }
 ];
