4583b723e955edc4f1e8907908f6cfb5d946b488
Joseph Paragas <josephparagas5@gmail.com>
2020-02-11 16:59:01 -1000

    staff page CSS

--- src/static_pages/StudioRental.jsx
+++ src/static_pages/StudioRental.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { default as HeadStuff } from '../reusables/HeadStuff';
+import { getUploadedImage } from '../utils';
 
 export default function(){
-
+  return [<HeadStuff title="Studio Rental" />,
+    <div className="studio-rental__content">
+      <h4 className="studio-rental__headline">
+        KTUH’s soundproof studios are perfect for recording podcasts,
+        music band sessions, etc. Studio time includes sound engineer:</h4>
+    </div>];
 }
