b4fa77b7f71f832a1ea58ff6d9d1a7cdebe063d5
josephpalma <joepalma08@hotmail.com>
2020-03-02 15:42:42 -1000

    jpfinal

--- src/podcasts/PodcastList.jsx
+++ src/podcasts/PodcastList.jsx
@@ -11,7 +11,6 @@ export default function PodcastList() {
     <div className='grid__container'>
       <h4>Student run podcasts from the only station that loves you. Streaming all the time on Apple Podcasts, Spotify, Soundcloud, Google Play and everywhere podcasts are heard.</h4>
       {podcasts.map(({ playlist_id: playlistId }) => <PodcastItem {...{ playlistId }} />)}
-      </div>, <div className='grid__item__submit'>
       <div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2020/03/The-Future-Accords.jpg" href={getFullUrl(`podcasts/thefutureaccords`)}/><a href={getFullUrl(`podcasts/thefutureaccords`)}><h4>The Future Accords</h4></a></div>
       <div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2019/06/ktuh-logo.jpg" href={getFullUrl(`podcasts/kwoktalk`)}/><a href={getFullUrl(`podcasts/kwoktalk`)}><h4>Kwok Talk</h4></a></div>
       <div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2019/06/ktuh-logo.jpg" href={getFullUrl(`podcasts/ktuhnews`)}/><a href={getFullUrl(`podcasts/ktuhnews`)}><h4>KTUH News</h4></a></div>
@@ -23,5 +22,5 @@ export default function PodcastList() {
       <div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2019/06/ktuh-logo.jpg" href={getFullUrl(`podcasts/talkshows`)}/><a href={getFullUrl(`podcasts/talkshows`)}><h4>Talk Shows</h4></a></div>
       </div>, <div className='grid__item__submit'><div className='submit__podcast'>
       <div className='submit__podcast'><h3><SamePageAnchor href={getFullUrl('submit-podcasts')}>
-  Submit a podcast</SamePageAnchor></h3></div></div>];
+        Submit a podcast</SamePageAnchor></h3></div></div></div>];
 }
