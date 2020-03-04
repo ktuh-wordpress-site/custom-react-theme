4fc11627ca7202f817dc7a6799b865bd8f7ef2c2
Joseph Paragas <josephparagas5@gmail.com>
2020-03-03 17:18:33 -1000

    changed format of Studio Rental page

--- src/static_pages/StudioRental.jsx
+++ src/static_pages/StudioRental.jsx
@@ -5,8 +5,18 @@ import { getUploadedImage } from '../utils';
 export default function(){
   return [<HeadStuff title="Studio Rental" />,
     <div className="studio-rental__content">
-      <h4 className="studio-rental__headline">
+      <h3 style= "textAlign: center" className="studio-rental__headline">
         KTUH’s soundproof studios are perfect for recording podcasts,
-        music band sessions, etc. Studio time includes sound engineer:</h4>
+        music band sessions, etc. Studio time includes sound engineer:</h3>
+
+      <div className="studio_img">
+        <img className="studio_img" src={getUploadedImage('studio-rental.jpg')} alt="" width="413" height="253" />
+        <p>$150/ hour recording</p>
+      </div>
+
+      <div className="studio_img">
+        <img className="studio_img" src={getUploadedImage('studio-rental 2.jpg')} alt="" width="413" height="253"/>
+        <p>KTUH affiliate discounts available</p>
+      </div>
     </div>];
 }
