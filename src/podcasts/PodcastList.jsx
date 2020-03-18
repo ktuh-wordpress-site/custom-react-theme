200b73baff834dc07f3af5c56783aeaa53cc5132
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2020-03-17 20:09:38 -1000

    Update PodcastList.jsx

--- src/podcasts/PodcastList.jsx
+++ src/podcasts/PodcastList.jsx
@@ -5,7 +5,8 @@ import { HeadStuff, SamePageAnchor } from '../reusables';
 export default function PodcastList() {
   return [<HeadStuff title="KTUH Podcasts" />,
     <h4 className="podcast-page__desc">Student run podcasts from the only station that loves you. Streaming all the time on Apple Podcasts, Spotify, Soundcloud, Google Play and everywhere podcasts are heard.</h4>,
-    <div className='grid__container'>
+      <div className="podcast-grid__cover">
+      <div className='grid__container'>
       <SamePageAnchor href={getFullUrl('podcasts/the-future-accords')}><div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2020/03/The-Future-Accords.jpg" href={getFullUrl('podcasts/the-future-accords')}/><SamePageAnchor href={getFullUrl('podcasts/the-future-accords')}><h4>The Future Accords</h4></SamePageAnchor></div></SamePageAnchor>
       <SamePageAnchor href={getFullUrl('podcasts/kwok-talk')}><div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2019/06/ktuh-logo.jpg" href={getFullUrl('podcasts/kwok-talk')}/><SamePageAnchor href={getFullUrl('podcasts/kwok-talk')}><h4>Kwok Talk</h4></SamePageAnchor></div></SamePageAnchor>
       <SamePageAnchor href={getFullUrl('podcasts/renegade-radio')}><div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2020/03/renegade-radio-logo-2020-compressed.png" href={getFullUrl('podcasts/renegade-radio')}/><SamePageAnchor href={getFullUrl('podcasts/renegade-radio')}><h4>Renegade Radio</h4></SamePageAnchor></div></SamePageAnchor>
@@ -14,7 +15,8 @@ export default function PodcastList() {
       <SamePageAnchor href={getFullUrl('podcasts/interviews')}><div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2019/06/ktuh-logo.jpg" href={getFullUrl('podcasts/interviews')}/><SamePageAnchor href={getFullUrl('podcasts/interviews')}><h4>Interviews</h4></SamePageAnchor></div></SamePageAnchor>
       <SamePageAnchor href={getFullUrl('podcasts/talk-shows')}><div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2019/06/ktuh-logo.jpg" href={getFullUrl('podcasts/talk-shows')}/><SamePageAnchor href={getFullUrl('podcasts/talk-shows')}><h4>Talk Shows</h4></SamePageAnchor></div></SamePageAnchor>
       <SamePageAnchor href={getFullUrl('podcasts/more-podcasts')}><div className="grid__item"><img className="podcast__image" src="https://manoa.hawaii.edu/ktuh/wp-content/uploads/2019/06/ktuh-logo.jpg" href={getFullUrl('podcasts/more-podcasts')}/><SamePageAnchor href={getFullUrl('podcasts/more-podcasts')}><h4>More Podcasts</h4></SamePageAnchor></div></SamePageAnchor>
-      </div>, <div className='grid__item__submit'><div className='submit__podcast'>
+      </div>,
+      <div className='grid__item__submit'>
       <div className='submit__podcast'><h3><SamePageAnchor href={getFullUrl('submit-podcasts')}>
-        Submit a podcast</SamePageAnchor></h3></div></div></div>];
+          Submit a podcast</SamePageAnchor></h3></div></div></div>];
 }
