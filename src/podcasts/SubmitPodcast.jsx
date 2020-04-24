63827ad2a22b9a61dd3a04d0fd4feb2f3f0be46a
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2020-04-23 16:47:56 -1000

    submit podcast css

--- src/podcasts/SubmitPodcast.jsx
+++ src/podcasts/SubmitPodcast.jsx
@@ -4,8 +4,8 @@ import EmailLink from '../includes/EmailLink';
 
 export default function SubmitPodcast() {
   return [<HeadStuff title="Submit Podcast" headerText="Submit a Podcast" />,
-    <h5 className="show__time">Do you want to hear your podcast on KTUH?</h5>,
-    <div className="show-item__genres">
+    <div className="submit-podcast__page">
+      <h5 className="show__time">Do you want to hear your podcast on KTUH?</h5>
       Please submit your sample to <EmailLink to="podcasts@ktuh.org" />.;
     </div>
   ];
