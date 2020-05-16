701a4b9fc78681fde553aa0e67e135e7331260e6
Derek Chan <dchan3@hawaii.edu>
2020-05-15 15:09:36 -1000

    Profile page and podcast page tweaks

--- src/dj_bios/ProfilePage.jsx
+++ src/dj_bios/ProfilePage.jsx
@@ -1,53 +1,53 @@
 import React from 'react';
-import {useSlug, useApiRequest} from '../hooks';
-import {default as HeadStuff} from '../reusables/HeadStuff';
-import {SocialButton} from './ProfileSocialLink';
-import {default as SamePageAnchor} from '../reusables/SamePageAnchor';
-import {BackButton, Glyph} from "../reusables";
+import { useSlug, useApiRequest } from '../hooks';
+import { default as HeadStuff } from '../reusables/HeadStuff';
+import { SocialButton } from './ProfileSocialLink';
+import { SamePageAnchor } from '../reusables';
+import { NotFoundRedirect } from '../utils';
 
 function ProfilePage() {
-    let slug = useSlug(), persona = useApiRequest(null, `persona_by_dj_slug?slug=${slug}`),
-        dj_bio = useApiRequest(null, `dj_bio_by_slug?slug=${slug}`, (data, fxn) => {
-            if (data && data.length) {
-                fxn(data[0]);
-            } else fxn(null);
-        });
+  let slug = useSlug(), persona = useApiRequest(undefined, `persona_by_dj_slug?slug=${slug}`),
+    dj_bio = useApiRequest(undefined, `dj_bio_by_slug?slug=${slug}`, (data, fxn) => {
+      if (data && data.length) {
+        fxn(data && data.length ? data[0] : null);
+      } else fxn(null);
+    });
 
-    if (persona && dj_bio) {
-        let {
-            bio, name, image
-        } = persona, {
-            website_url, show_url, soundcloud_link: [soundcloud_link],
-            instagram_link: [instagram_link], twitter_link: [twitter_link],
-            facebook_link: [facebook_link]
-        } = dj_bio;
-        return [<HeadStuff title={`${name}'s Profile`}
-                           description={`${name}'s Profile`} image={image}/>,
-            <div>{show_url && show_url[0].length ?
-                <BackButton href={show_url[0]} className='show__link' text="Show Page"/> : null}</div>,
-            <div className="profile__image-div">
-                <img className='profile__pic' src={image}/>
-            </div>,
-            <div className='profile'>
-                <div className='profile__left'>
-                    <div className='profile__name'>
-                        <h5>{name}</h5>
-                    </div>
-                    {website_url.length ? <div className='show-links'>
-                        <a href={website_url}><Glyph symbol='globe' type='fa'/></a>
-                        {soundcloud_link.length ? <a href={soundcloud_link}><Glyph symbol='soundcloud' type='fa'/></a> : null}
-                        {instagram_link.length ? <a href={instagram_link}><Glyph symbol='instagram' type='fa'/></a> : null}
-                        {twitter_link.length ? <a href={twitter_link}><Glyph symbol='twitter' type='fa'/></a> : null}
-                        {facebook_link.length ? <a href={facebook_link}><Glyph symbol='facebook' type='fa'/></a> : null}
-                    </div>}
-                    <div className='profile__bio' dangerouslySetInnerHTML={{
-                        __html: bio || `<i>(${name} hasn't filled out a bio yet.)</i>`
-                    }}/>
-                </div>
-            </div>];
-    }
+  if (persona && dj_bio) {
+    let {
+        bio, name, image
+      } = persona, {
+        website_url, show_url, soundcloud_link: [soundcloud_link],
+        instagram_link: [instagram_link], twitter_link: [twitter_link],
+        facebook_link: [facebook_link]
+      } = dj_bio;
+    return [<HeadStuff title={`${name}'s Profile`}
+      description={`${name}'s Profile`} image={image}/>,
+      <div>{show_url && show_url[0].length
+        ? <SamePageAnchor href={show_url[0]} className='show__link' text="Show Page" /> : null}</div>,
+      <div className="profile__image-div">
+        <img className='profile__pic' src={image}/>
+      </div>,
+      <div className='profile'>
+          <div className='profile__left'>
+              <div className='profile__name'>
+                  <h5>{name}</h5>
+              </div>
+              <div className='show-links'>
+              {website_url.length ? <SocialButton url={website_url} site='globe' /> : null}
+              {soundcloud_link.length ? <SocialButton url={soundcloud_link} site='soundcloud' /> : null}
+              {instagram_link.length ? <SocialButton url={instagram_link} site='instagram' /> : null}
+              {twitter_link.length ? <SocialButton url={twitter_link} site='twitter' /> : null}
+              {facebook_link.length ? <SocialButton url={facebook_link} site='facebook' /> : null}
+              </div>
+              <div className='profile__bio' dangerouslySetInnerHTML={{
+                __html: bio || `<i>(${name} hasn't filled out a bio yet.)</i>`
+              }}/>
+          </div>
+      </div>];
+  }
 
-    return null;
+  return <NotFoundRedirect check={persona} />;
 }
 
 export default ProfilePage;
--- src/podcasts/PodcastItem.jsx
+++ src/podcasts/PodcastItem.jsx
@@ -1,51 +1,56 @@
 import React from 'react';
-import {useSlug} from '../hooks/useGeneralContext';
-import {default as IThing} from '../reusables/IThing';
-import {queryToUrl, getMagicFieldsImg, getUploadedImage} from '../utils/url_utils';
-import {default as useApiRequest} from '../hooks/useApiRequest';
-import {HeadStuff} from '../reusables';
+import { useSlug } from '../hooks/useGeneralContext';
+import { default as IThing } from '../reusables/IThing';
+import { queryToUrl, getMagicFieldsImg, getUploadedImage } from '../utils/url_utils';
+import { default as useApiRequest } from '../hooks/useApiRequest';
+import { HeadStuff, BackButton } from '../reusables';
+import { NotFoundRedirect } from '../utils';
 
 export default function PodcastItem() {
-    let slug = useSlug(), podcast = useApiRequest(null,
-        `podcast_series?slug=${slug}`, function (data, fxn) {
-            fxn(data[0]);
-        });
+  let slug = useSlug(), podcast = useApiRequest(undefined,
+    `podcast_series?slug=${slug}`, function (data, fxn) {
+      fxn((data && data.length) ? data[0] : null);
+    });
 
-    if (podcast) {
-        let {
-                playlist_id: playlistId, title, description, time, photo, host
-            } = podcast,
-            src = 'https://w.soundcloud.com/player/?' + queryToUrl({
-                url: `https://api.soundcloud.com/playlists/${playlistId}`,
-                color: '#ff5500',
-                auto_play: false,
-                hide_related: false,
-                show_comments: true,
-                show_user: true,
-                show_reposts: false,
-                show_teaser: true,
-                visual: true
-            });
+  if (podcast) {
+    let {
+        playlist_id: playlistId, title, description, time, photo, host
+      } = podcast, src = `https://w.soundcloud.com/player/?${queryToUrl({
+        url: `https://api.soundcloud.com/playlists/${playlistId}`,
+        color: '#ff5500',
+        auto_play: false,
+        hide_related: false,
+        show_comments: true,
+        show_user: true,
+        show_reposts: false,
+        show_teaser: true,
+        visual: true
+      })}`;
 
-      return [<HeadStuff title={title}/>, <div className="podcast__page-item">
-          <div className="podcast__photo-div">
-              <img className="podcast-page__photo" src={getMagicFieldsImg(photo) || getUploadedImage('2019/06/ktuh-logo.jpg')}/>
-          </div>
-            <div className="podcast__item-info">
-                <div className="podcast__time-div">
-                    <h5 className="podcast__time">{time || new Date().toString()}</h5>
-                </div>
-                {(host && host.length)
-                ?
-                <div className="podcast__host-div">
-                    <h4 className="podcast__host">{`Hosted by ${host}`}</h4>
-                </div> : null}
-                <div className="podcast__description-div">
-                    <p className="podcast__description">{description || 'This is a pretty cool podcast! from KTUH! Check it out!'}</p>
-                </div>
+    return [<HeadStuff title={title}/>,
+      <BackButton href='podcasts' className='show__link' text="Podcasts" />,
+      <div className="show__wrapper">
+        <div className="show__content">
+          <div className="show__image-div">
+            <img className="show__image"
+              src={(photo && photo.length) && getMagicFieldsImg(photo)
+              || getUploadedImage('2019/06/ktuh-logo.jpg')}/>
+            <div className="show__info">
+              {(host && host.length) ? <h5>Hosted by {host}</h5> : null}
+                  <h5 className="show__time">{time || new Date().toString()}</h5>
+              <div>
+              {description ? <h5>Podcast Description</h5> : null}
+              <div className="show__body" dangerouslySetInnerHTML=
+                {{ __html: description }} />
             </div>
-            <div className="podcast__playlist"><IThing height="450" width="800" {...{src}} /></div></div>
-        ];
-    }
-    return null;
+          </div>
+        </div>
+        <div className="show-page__playlist">
+          <h4>Past Episodes</h4>
+          <IThing height="450" width="800" {...{ src }} />
+        </div>
+      </div>
+    </div>];
+  }
+  return <NotFoundRedirect check={podcast} />;
 }
