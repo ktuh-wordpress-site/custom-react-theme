ee99eab2dac39b8a22ea4a7a106caff482bd590e
Derek Chan <dchan3@hawaii.edu>
2020-05-21 20:19:16 -1000

    Podcast display tweaks

--- src/podcasts/PodcastListItem.jsx
+++ src/podcasts/PodcastListItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { getFullUrl, getUploadedImage } from '../utils/url_utils';
+import { getFullUrl, getUploadedImage, getMagicFieldsImg } from '../utils/url_utils';
 import { SamePageAnchor } from '../reusables';
 
 export default function PodcastListItem({ href, src = '2019/06/ktuh-logo.jpg', name }) {
   return <SamePageAnchor href={getFullUrl(`podcasts/${href}`)}>
     <div className="grid__item">
-      <img className="podcast__page-image" src={getUploadedImage(src)}
+      <img className="podcast__page-image" src={(src.length && src !== '2019/06/ktuh-logo.jpg')
+        ? getMagicFieldsImg(src) : getUploadedImage('2019/06/ktuh-logo.jpg')}
         href={getFullUrl(`podcasts/${href}`)} />
       <SamePageAnchor href={getFullUrl(`podcasts/${href}`)}>
         <h4>{name}</h4>
