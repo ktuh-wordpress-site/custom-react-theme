e85fd20332f1e933a077bd372879d15db9b79ecc
Derek Chan <dchan3@hawaii.edu>
2020-07-02 13:03:04 -1000

    Review list item title tweak

--- src/reviews/ReviewItem.jsx
+++ src/reviews/ReviewItem.jsx
@@ -12,7 +12,9 @@ function ReviewItem({
   return <div className='review-item'>
     <SamePageAnchor href={getFullUrl(`reviews/${slug}`)}>
       <img className='review-item__image' {...{ src }} />
-      <div className='review-item__release'>{title.rendered || title[0]}</div>
+      <div className='review-item__release'>{(title.rendered || title[0]).replace(/&#(\d+);/, function (match, p1) {
+        return String.fromCharCode(parseInt(p1, 10));
+      })}</div>
       <div className='review-item__artist'>{artist}</div>
     </SamePageAnchor>
   </div>;
