1c4d7d371485f347179bb870f0b5f5ad4c8de09c
Derek Chan <dchan3@hawaii.edu>
2020-07-17 16:41:28 -1000

    Chart item icon render

--- src/charts/ChartItem.jsx
+++ src/charts/ChartItem.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { SamePageAnchor, Glyph } from '../reusables';
 import { getFullUrl } from '../utils/url_utils';
 
-export default function ChartItem({chart: {slug, title: { rendered: title }}}) {
-    return <div className="charts__sidebar">
-            <ul>
-                <a href={getFullUrl(`charts/${slug}`)}><Glyph symbol="cd" type='fa' /></a> <SamePageAnchor href={getFullUrl(`charts/${slug}`)}>{title}</SamePageAnchor>
-            </ul>
-    </div>
+export default function ChartItem({ chart: { slug, title: { rendered: title } } }) {
+  return <div className="charts__sidebar">
+      <ul>
+        <SamePageAnchor href={getFullUrl(`charts/${slug}`)}><Glyph symbol="cd" /></SamePageAnchor>&nbsp;
+        <SamePageAnchor href={getFullUrl(`charts/${slug}`)}>{title}</SamePageAnchor>
+      </ul>
+    </div>;
 }
