70f799a5b71297034d14cc5414676851ba0b6839
Derek Chan <dchan3@hawaii.edu>
2020-07-21 18:03:10 -1000

    Charts tweaks

--- src/charts/ChartItem.jsx
+++ src/charts/ChartItem.jsx
@@ -6,7 +6,7 @@ export default function ChartItem({ chart: { slug, title: { rendered: title } } 
   return <li>
     <div className="charts__sidebar">
       <SamePageAnchor href={getFullUrl(`charts/${slug}`)}><Glyph symbol="cd" /></SamePageAnchor>&nbsp;
-      <SamePageAnchor href={getFullUrl(`charts/${slug}`)}>{title}</SamePageAnchor>
+      <SamePageAnchor href={getFullUrl(`charts/${slug}`)}>{title.replace('NACC Charts ', '')}</SamePageAnchor>
     </div>
   </li>;
 }
--- src/charts/ChartList.jsx
+++ src/charts/ChartList.jsx
@@ -12,7 +12,7 @@ export default function () {
     return [<HeadStuff title='Charts' />,
       <div className="page__content">
         <div className="playlist-list__latest">
-          <h3 className="playlist__show-name">Latest Chart: {charts[0].title.rendered}</h3>
+          <h3 className="playlist__show-name">Latest Chart: {charts[0].title.rendered.replace('NACC Charts ', '')}</h3>
           <ChartTable data={charts[0].chart_table[0] } />
         </div>
         <div className="playlist__sidebar">
