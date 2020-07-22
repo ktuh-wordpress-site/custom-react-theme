a2bce12c2d1e06b736e7abeed212691960882418
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2020-07-21 18:58:41 -1000

    charts push

--- src/charts/ChartItem.jsx
+++ src/charts/ChartItem.jsx
@@ -3,10 +3,10 @@ import { SamePageAnchor, Glyph } from '../reusables';
 import { getFullUrl } from '../utils/url_utils';
 
 export default function ChartItem({ chart: { slug, title: { rendered: title } } }) {
-  return <li>
+  return <li className="charts__list-item">
     <div className="charts__sidebar">
-      <SamePageAnchor href={getFullUrl(`charts/${slug}`)}><Glyph symbol="cd" /></SamePageAnchor>&nbsp;
-      <SamePageAnchor href={getFullUrl(`charts/${slug}`)}>{title.replace('NACC Charts ', '')}</SamePageAnchor>
+      <SamePageAnchor href={getFullUrl(`charts/${slug}`)}><Glyph id="charts__item-icon" symbol="cd" /></SamePageAnchor>
+      &nbsp;<SamePageAnchor href={getFullUrl(`charts/${slug}`)}>{title.replace('NACC Charts ', '')}</SamePageAnchor>
     </div>
   </li>;
 }
--- src/charts/ChartList.jsx
+++ src/charts/ChartList.jsx
@@ -9,17 +9,32 @@ export default function () {
   let charts = useApiRequest([], 'chart');
 
   if (charts.length) {
-    return [<HeadStuff title='Charts' />,
+    return [<HeadStuff title='NACC Charts' />,
       <div className="page__content">
+        <div className="charts__bio">
+          <h4>The North American College & Community Chart</h4>
+          <div className="charts__text">The North American College & Community Chart (NACC) represents the top 30 most
+            played albums at KTUH over a seven day period. Every week, Music Directors from college radio stations
+            around the country send their charts to the NACC where they are compiled weekly into a national
+            <a className="charts__link" href="https://naccchart.com/charts"> chart. </a>
+            NACC’s top 200 chart provides a broad view of albums popular at college and
+            community stations across the country. NACC also publishes
+            <a className="charts__link" href="https://naccchart.com/community"> profiles </a>
+            of Music Directors and all the people that keep the public radio machine running. For more
+            information on the NACC organization and national charts contributed to by KTUH, please visit the
+            <a className="charts__link" href="https://naccchart.com/"> NACC website. </a>
+          </div>
+        </div>
         <div className="playlist-list__latest">
           <h3 className="playlist__show-name">Latest Chart: {charts[0].title.rendered.replace('NACC Charts ', '')}</h3>
           <ChartTable data={charts[0].chart_table[0] } />
         </div>
+        <div className="charts__pad"></div>
         <div className="playlist__sidebar">
           <h3>Other Charts</h3>
           <div className="charts__list">
             <ul>
-              {charts.slice(1).map(chart => <ChartItem chart={chart}/>)}
+              {charts.slice(1, 13).map(chart => <ChartItem chart={chart}/>)}
             </ul>
           </div>
         </div>
