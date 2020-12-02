29acf93cebbfd1bc6a326cf62ad335b2fccd0dcd
Derek Chan <dchan3@hawaii.edu>
2020-12-01 14:50:09 -1000

    Patch for automated show creation when not found

--- php/types/now_playing/rest.php
+++ php/types/now_playing/rest.php
@@ -39,6 +39,30 @@ register_rest_field('now_playing', 'artist', array(
           'methods' => 'POST',
           'callback' => function (WP_REST_Request $request) {
               update_now_playing($request['artist'], $request['song'], $request['show']);
+
+              $ps = get_posts(array(
+                'posts_per_page' => -1,
+                'post_type' => 'wpspin_profiles',
+                'post_status' => 'publish',
+                'meta_query' => array(
+                  array(
+                     'key'     => 'show_page_id',
+                     'value'   => $request['showId']
+                  )
+                )
+              ));
+
+              if (!$ps) {
+                $id = wp_insert_post(
+                    array(
+                      'post_author' => 1,
+                      'post_type' => 'wpspin_profiles',
+                      'post_title' => $request['show'],
+                      'post_status' => 'publish'
+                    )
+                );
+                update_post_meta($id, 'show_page_id', $request['showId']);
+              }
           }
       )
   ));
