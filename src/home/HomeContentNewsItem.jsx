517cf9701f827edc054f6be5d2c2827af8e887a9
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2021-11-08 15:10:07 -1000

    homepage formatting

--- src/home/HomeContentNewsItem.jsx
+++ src/home/HomeContentNewsItem.jsx
@@ -11,6 +11,13 @@ export default function HomeContentNewsItem({
 }) {
   let src = getFeaturedImg(_embedded, getImgAsset('mstile-310x310.png'));
 
+  if (title.includes('&#8216;', '&#8217;', '&#038', '&#8211;')) {
+    title = title.replace('&#8216;', "'")
+      .replace('&#8217;', "'")
+      .replace('&#038;', '&')
+      .replace('&#8211;', '-');
+  }
+
   return <div className='home__news-item'>
     <SamePageAnchor href={getFullUrl(`radioblog/${slug}`)}><img
       className='home__news-img' src={src} />
